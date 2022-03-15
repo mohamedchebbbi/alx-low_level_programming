2ea70652d12a5fb45e6936050482a756a3e5ec57
mohamed chebbi <mohamedchebbbi@gmail.com>
2022-03-15 21:24:59 +0100

    fix task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,51 +1,47 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints the first 98 number
+ * main - calls fibonacci
  *
- * Description: fibobnachi numbers starting from one
- * Return: 0
+ * Return: always 0
  */
-
 int main(void)
 {
-	long fib1, fib1a, fib1b, fib2, fib2a, fib2b, fib3, fib3a, fib3b;
-	long divi = 10000000000;
-
-	fib1 = 0;
-	fib2 = 1;
-	int counter = 1;
-
-	for (counter = 0; counter < 91; counter++)
-	{
-		fib3 =  fib2 + fib1;
-		fib1 = fib2;
-		fib2 = fib3;
-		printf("%ld, ", fib3);
-	}
-	fib1a = fib1 / divi;
-	fib1b = fib1 % divi;
-	fib2a = fib2 / divi;
-	fib2b = fib2 % divi;
-	for (; counter < 98; counter++)
-	{
-		fib3a = fib1a + fib2a;
-		fib3b = fib1b + fib2b;
-		if (fib3b > divi)
-		{
-			fib3b = fib3b % divi;
-			fib3a++;
-		}
-		if (counter != 97)
-		{
-			printf("%ld%ld, ", fib3a, fib3b);
-			fib1a = fib2a;
-			fib1b = fib2b;
-			fib2a = fib3a;
-			fib2b = fib3b;
-		}
-		else
-			printf("%ld%ld\n", fib3a, fib3b);
-	}
+	fibonacci_print_98();
 	return (0);
+}
+
+/**
+ * fibonacci_print_98 - print first 98 fibonacci numbers
+ *
+ * Return: void
+ */
+void fibonacci_print_98(void)
+{
+	unsigned long i, a1, a2, b1, b2, c1, c2, m, r;
+
+	m = 1000000000000000000;
+	a1 = 0;
+	a2 = 1;
+	b1 = 0;
+	b2 = 2;
+	for (i = 0; i < 98; i++)
+	{
+		if (a1)
+			printf("%lu", a1);
+		printf("%lu", a2);
+		if (i < 97)
+			printf(", ");
+		c1 = a1;
+		c2 = a2;
+		a1 = b1;
+		a2 = b2;
+		b1 += c1;
+		b2 += c2;
+		r = b2 / m;
+		b2 %= m;
+		b1 += r;
+	}
+	printf("\n");
 }
