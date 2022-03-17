40195f9b37ef45f6d5ff8a20e052330ea448a6c5
mohamed chebbi <mohamedchebbbi@gmail.com>
2022-03-17 21:37:29 +0100

    task 0 to 10

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "notrebloh.h"
+
+/**
+ * print_diagonal - print backslash diagonally
+ * @n: int type
+ *
+ * Return: prints lines and space to the value of n
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 1; a <= n; a++)
+	{
+		for (b = 2; b <= a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n < 1)
+		_putchar('\n');
+}
