13391066d3e92192a66d00f38b56c5092045f3d8
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-03-17 14:25:17 -0700

    task 3 to 10

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - print a line n characters long
+ * @n: the length of the line
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
