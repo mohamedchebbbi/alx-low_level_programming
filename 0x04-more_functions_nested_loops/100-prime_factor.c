f60bae204c4d1de01eb1c8deb2fa9fb32fdd10d6
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-03-17 14:30:41 -0700

    task 11 and 12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+#define NUM 612852475143
+
+/**
+ * main - find the largest prime factor of 612852475143
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	long i = 2, p = 0, n = NUM;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (!(n % i) && is_prime(i))
+		{
+			n /= i;
+			if (i > p)
+			{
+				p = i;
+			}
+			if (is_prime(n))
+			{
+				p = n;
+				break;
+			}
+			i = 2;
+		}
+	}
+	printf("%ld\n", p);
+	return (0);
+}
+
+/**
+ * is_prime - checks if an integer is prime
+ * @n: the integer to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime(long n)
+{
+	int i;
+
+	if (n < 2)
+		return (0);
+	for (i = 2; i <= sqrt(n); i++)
+		if (!(n % i))
+			return (0);
+	return (1);
+}
