56957676bef51aa167eb4d763d698aeeff6e1ce5
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-03-21 18:21:59 -0700

    task 1 to 9

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse with newline
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (--i >= 0)
+		_putchar(s[i]);
+	_putchar('\n');
+}
