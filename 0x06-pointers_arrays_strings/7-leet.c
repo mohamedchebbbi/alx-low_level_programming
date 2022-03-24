d24263b529c91256321daa8c91a76d54504806f2
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-03-23 21:49:09 -0700

    task 0 to 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#define LEETIN "aAeEoOtTlL"
+#define LEETOUT "4433007711"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to encode
+ *
+ * Return: char pointer
+ */
+char *leet(char *s)
+{
+	char *ret = s, *leetin = LEETIN, *leetout = LEETOUT;
+	int i = 0;
+
+	for (; *s; s++)
+	{
+		for (i = 0; leetin[i]; i++)
+			if (*s == leetin[i])
+				*s = leetout[i];
+	}
+	return (ret);
+}
