5352e5af07a1b09549041b41fd667870bb9a222f
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-03-30 17:51:20 -0700

    task 0 to 6

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _puts_recursion - prints a string followed by newline recursively
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
