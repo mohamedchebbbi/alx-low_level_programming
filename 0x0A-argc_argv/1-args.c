b51bdc578504a92ba7d678b3da911b4db455b7cd
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-04-01 10:54:35 -0700

    task 0 to 4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - it all starts here
+ * @argc: the number of arguments
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
