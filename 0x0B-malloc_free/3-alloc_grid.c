bb23ad8e6091fb13c90b074e4a11944512f740b8
mohamed <mohamedchebbbi@gmail.comgit config --global user.name mohamed>
2022-04-05 16:41:59 -0700

    task 0to 4

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - allocates a two dimensional array
+ * @width: the table width
+ * @height: the table height
+ *
+ * Return: pointer to the table
+ */
+int **alloc_grid(int width, int height)
+{
+	int **rows, **ret;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ret = rows = malloc(sizeof(int *) * height);
+	if (!rows)
+		return (NULL);
+	while (height--)
+	{
+		int *cols = malloc(sizeof(int) * width);
+		int i = width;
+
+		if (!cols)
+		{
+			i = 0;
+			while (ret + i != rows)
+				free(ret[i++]);
+			free(ret);
+			return (NULL);
+		}
+		while (i--)
+			*cols++ = 0;
+		*rows++ = cols - width;
+	}
+	return (ret);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free an a previously allocated grid
+ * @grid: pointer to grid
+ * @height: the grid height
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	if (!grid || height <= 0)
+		return;
+	while (height--)
+		if (grid[height])
+			free(grid[height]);
+	free(grid);
+}
